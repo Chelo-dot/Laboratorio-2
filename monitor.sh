9ba2216ef9650c58a5f9cd82ccce1d646e5b13ff
Chelo-dot <marvillo028@gmail.com>
2026-04-14 16:09:50 -0600

    Avance ejercicio 2 falta agregar tiempo

--- monitor.sh
+++ monitor.sh
@@ -6,6 +6,7 @@ intervalo=2
 #Manejo de Argumentos
 
 if [ $# -eq 2 ]; then
+
     intervalo="${@: -1}"
 
 elif [ $# -eq 0 ] || [ $# -gt 2 ]; then
@@ -26,16 +27,20 @@ cleanup(){
     kill -9 $PID
 
     #Graficación 
+    TITULO="${comando} $PID"
 
     gnuplot <<EOF
     set terminal png
+    set xrange [0:$time]
+    set xtics 0, $intervalo, $time
     set output "monitor_$PID.png"
-    set title "PID $PID"
-    set xlabel "Tiempo"
+    set title "$TITULO"
+    set xlabel "Tiempo(s)"
     set ylabel "%CPU"
-    set y2label "RSS (KB)"
+    set y2label "RSS(KB)"
     set ytics nomirror
     set y2tics
+
     plot "monitor_$PID.log" using 3 with lines axes x1y1 title "CPU", "monitor_$PID.log" using 5 with lines axes x1y2 title "RSS"
 
 EOF
@@ -54,12 +59,12 @@ do
     TIMESTAMP=$(date "+%Y-%m-%d %I:%M:%S")
     echo "$TIMESTAMP $INFO" >> monitor_$PID.log
     time=$((time + intervalo))
+
     sleep $intervalo
 
 done
 
-TITULO="${comando} $PID"
-OUTPUT="monitor_${PID}.png"
+
 
 
 
