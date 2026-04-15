09e45060a4910fb6f25b3c9fb1a57798b7d59383
Chelo-dot <marvillo028@gmail.com>
2026-04-15 15:30:14 -0600

    Archivo para el servicio se añadio que guarde de manera correcta el archivo .log

--- monitor_servicio.sh
+++ monitor_servicio.sh
@@ -3,10 +3,10 @@
 
 while (true)
 do
-    INFO=$(ps -e --sort=%cpu -o cmd,%cpu,%mem,rss --no-headers | tail -n 5)
+    INFO=$(ps -e --sort=-%cpu -o cmd,%cpu,%mem,rss --no-headers | head -n 5)
     TIMESTAMP=$(date "+%Y-%m-%d %I:%M:%S")
-    echo "$TIMESTAMP" >> monitor_servicio.log
-    echo "$INFO" >> monitor_servicio.log
+    echo "$TIMESTAMP" >> /var/log/monitor_servicio.log
+    echo "$INFO" >> /var/log/monitor_servicio.log
 
     sleep 5
 
